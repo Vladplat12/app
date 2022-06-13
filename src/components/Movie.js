1850369a1d40af1bda1f7976f886fd93b7e6a385
Vladplat12 <Vladplat12@gmail.com>
2022-06-13 15:52:12 +0300

    navbar

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,36 +5,38 @@ import { Link } from "react-router-dom";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <Link
-      to={{
-        pathname: `/movie-details/${id}`,
-        state: {
-          year,
-          title,
-          summary,
-          poster,
-          genres,
-        },
-      }}
-    >
-      <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie_column">
-          <h3 className="movie_title">{title}</h3>
-          <h5 className="movie_year">{year}</h5>
-          <ul className="movie_genres">
-            {genres.map((genre, index) => {
-              return (
-                <li key={index} className="genres_genre">
-                  {genre}
-                </li>
-              );
-            })}
-          </ul>
-          <p className="movie_summury">{summary.slice(0, 140)}...</p>
+    <div className="container-fluid">
+      <Link
+        to={{
+          pathname: `/movie-details/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <div className="movie">
+          <img src={poster} alt={title} title={title} />
+          <div className="movie_column">
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <ul className="movie_genres">
+              {genres.map((genre, index) => {
+                return (
+                  <li key={index} className="genres_genre">
+                    {genre}
+                  </li>
+                );
+              })}
+            </ul>
+            <p className="movie_summury">{summary.slice(0, 140)}...</p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,27 +4,34 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand>logo</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link>
-            <Link to="/">Home</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/about">About</Link>
-          </Nav.Link>
-        </Nav>
-        <Nav>
-          <Button variant="primary" className="mr-2">
-            Log In
-          </Button>
-          <Button variant="primary">Sign Out</Button>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-
+    <div Container>
+      <Navbar
+        className="navbar fixed-top "
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand>logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Button variant="primary" className="mr-2">
+              Log In
+            </Button>
+            <Button variant="primary">Sign Out</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
     // <div>
     //   <Link to="/">Main</Link>
     //   <Link to={{ pathname: "/about", state: { fromLocation: true } }}>
